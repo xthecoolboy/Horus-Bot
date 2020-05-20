f5ed8ebe574fa99d8a54bb56ab004d28744b11c1
Luiz Eduardo <luizdudumr@gmail.com>
2020-05-20 10:49:31 -0300

    Volume command fix

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -1,15 +1,22 @@
 module.exports = {
-	name: 'volume',
-	description: 'Comando de volume!.',
+	name: "volume",
+	description: "Comando de volume!.",
 	cooldown: 5,
-	execute(client, msg) {
-		const { channel } = message.member.voice;
-		if (!channel) return message.channel.send('Você precisa estar em um canal de música para ouvir música!');
-		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('Não há nada tocando agora.');
-		if (!args[0]) return message.channel.send(`O volume atual é: **${serverQueue.volume}**`);
-		serverQueue.volume = args[0]; // eslint-disable-line
-		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-		return message.channel.send(`Alterei o volume para: **${args[0]}**`);
+	execute(client, message, args) {
+	  const { channel } = message.member.voice;
+	  if (!channel)
+		return message.channel.send(
+		  "Você precisa estar em um canal de música para ouvir música!"
+		);
+	  const serverQueue = message.client.queue.get(message.guild.id);
+	  if (!serverQueue) return message.channel.send("Não há nada tocando agora.");
+	  if (!args[0])
+		return message.channel.send(
+		  `O volume atual é: **${serverQueue.volume}**`
+		);
+	  serverQueue.volume = args[0]; // eslint-disable-line
+	  serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
+	  return message.channel.send(`Alterei o volume para: **${args[0]}**`);
 	}
-};
+  };
+  
